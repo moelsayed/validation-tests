87e6474d777bf2af575c3444f65e353149e14440
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2017-07-28 18:51:58 +0200

    add loop info

--- tests/v2_validation/cattlevalidationtest/core/test_stress_forceupgrade.py
+++ tests/v2_validation/cattlevalidationtest/core/test_stress_forceupgrade.py
@@ -78,7 +78,10 @@ def validate_kubectl():
 @if_stress_testing
 def test_upgrade_validate_k8s(kube_hosts, rancher_cli_container):
     for i in range(1, upgrade_loops):
+        logger.info("Starting Loop: " + str(i))
+        print 'loop: ' + str(i)
         force_upgrade_stack("ipsec")
         waitfor_infra_stacks()
         time.sleep(300)
         assert validate_kubectl()
+        logger.info("End of Loop: " + str(i))
